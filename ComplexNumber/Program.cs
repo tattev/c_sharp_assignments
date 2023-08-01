2f0991a1a917bed6358d9dd63d5ef861c49cbda3
Tatevik Mkrtchyan <89747511+tattev@users.noreply.github.com>
2023-08-02 01:19:31 +0400

    Added project

---
+++ ComplexNumber/Program.cs
@@ -0,0 +1,104 @@
+﻿/*  Complex Number
+    Requirements
+    ----------------------
+    Implement a class that represents a complex number along with all the basic operations
+    Implement a method to return the absolute value of the current number
+    Overload plus, minus, and multiply by a scalar operators
+    Overload implicit and explicit cast operators for the real part of the number
+    Overload ToString method to return the string representation of the current number
+*/
+
+    public class ComplexNumber
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+
+        public ComplexNumber(double real = 0.0, double imaginary = 0.0)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        //a method to return the absolute value of the current number
+        public double Absolute()
+        {
+            return Math.Sqrt(Real * Real + Imaginary * Imaginary);
+        }
+
+        //plus operator
+        public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
+        {
+            return new ComplexNumber(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+        }
+
+        //minus operator
+        public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2)
+        {
+            return new ComplexNumber(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
+        }
+
+        // multiply by a scalar operators
+        public static ComplexNumber operator *(ComplexNumber c, double scalar)
+        {
+            return new ComplexNumber(c.Real * scalar, c.Imaginary * scalar);
+        }
+
+        public static ComplexNumber operator *(double scalar, ComplexNumber c)
+        {
+            return c * scalar;
+        }
+
+        //implicit cast operator for the real part of the number
+        public static implicit operator double(ComplexNumber c)
+        {
+            return c.Real;
+        }
+
+        //explicit cast operator for the real part of the number
+        public static explicit operator ComplexNumber(double real)
+        {
+            return new ComplexNumber(real, 0);
+        }
+
+        //ToString method to return the string representation of the current number
+        public override string ToString()
+        {
+            if (Imaginary >= 0)
+            {
+                return $"{Real} + {Imaginary}i";
+            }
+            else
+            {
+                return $"{Real} - {-Imaginary}i";
+            }
+        }
+
+}
+
+    class Program
+    {
+        static void Main()
+        {
+            ComplexNumber c1 = new ComplexNumber(2, 3);
+            ComplexNumber c2 = new ComplexNumber(4, 5);
+
+            ComplexNumber c3 = c1 + c2;
+            Console.WriteLine(c3.ToString());  // Output: 6 + 8i
+
+            ComplexNumber c4 = c2 - c1;
+            Console.WriteLine(c4.ToString());  // Output: 2 + 2i
+
+            ComplexNumber c5 = c1 * 2;
+            Console.WriteLine(c5.ToString());  // Output: 4 + 6i
+
+            double realPart = c1;
+            Console.WriteLine(realPart.ToString());  // Output: 2
+
+            ComplexNumber c6 = (ComplexNumber)3.5;
+            Console.WriteLine(c6.ToString());  // Output: 3.5 + 0i
+
+            Console.ReadLine();
+        }
+    }
+
+
---
+++ CustomStringBuilderLinkedList/Program.cs
@@ -0,0 +1,199 @@
+﻿
+public class CustomStringBuilderLinkedList
+{
+    private class Node
+    {
+        public char Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(char data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+    private Node? head;
+    private Node? tail;
+    private int length;
+
+
+    //Length method that returns the current length of the string being built.
+    public int Length
+    {
+        get { return length; }
+    }
+
+    private CustomStringBuilderLinkedList Append(char c)
+    {
+        Node newNode = new Node(c);
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.Next = newNode;
+            tail = newNode;
+        }
+
+        length++;
+        return this;
+    }
+    //Append method, which appends the given string to the end of the current string.
+    public CustomStringBuilderLinkedList Append(string str)
+    {
+        if (str == null)
+            return null;
+
+        foreach (char c in str)
+        {
+            Append(c);
+        }
+        return this;
+    }
+
+    //InsertAt method, which inserts the given string at the specified index in the current string.
+    private CustomStringBuilderLinkedList InsertAt(char c, int index)
+    {
+        Node newNode = new Node(c);
+        if (index == 0)
+        {
+            newNode.Next = head;
+            head = newNode;
+        }
+        else
+        {
+            Node current = head;
+            for (int i = 0; i < index - 1; i++)
+            {
+                current = current.Next;
+            }
+            newNode.Next = current.Next;
+            current.Next = newNode;
+        }
+
+        length++;
+        return this;
+    }
+
+    public CustomStringBuilderLinkedList InsertAt(string str, int index)
+    {
+        if (str == null || index < 0 || index > length)
+            return null;
+
+        foreach (char c in str)
+        {
+            InsertAt(c, index++);
+        }
+        return this;
+    }
+
+
+    //RemoveDuplicates method, which removes duplicate characters from the current string, leaving only the first occurrences.
+    public CustomStringBuilderLinkedList RemoveDuplicates()
+    {
+        Node current = head;
+        while (current != null)
+        {
+            Node node = current;
+            while (node.Next != null)
+            {
+                if (node.Next.Data == current.Data)
+                {
+                    node.Next = node.Next.Next;
+                    length--;
+                }
+                else
+                {  
+                    node = node.Next;
+                }
+            }
+            current = current.Next;
+         }
+        return this;
+    }
+
+    //RemoveWhitespaces method, which removes all whitespace characters from the current string.
+    public CustomStringBuilderLinkedList RemoveWhitespaces()
+    {
+        Node current = head;
+        while (current != null)
+        {
+            if (char.IsWhiteSpace(current.Data))
+            {
+                head = current.Next;
+                current = head;
+                length--;
+            }
+            else if (current.Next != null && char.IsWhiteSpace(current.Next.Data))
+            {
+                current.Next = current.Next.Next;
+                length--;
+            }
+            else
+            {
+                current = current.Next;
+            }      
+        }
+        return this;
+    }
+
+    //GetString method, which returns the string representation of the current string.
+    public string GetString()
+    {
+        char[] result = new char[length];
+        Node current = head;
+        int index = 0;
+        while (current != null)
+        {
+            result[index++] = current.Data;
+            current = current.Next;
+        }
+        return new string(result);
+    }
+
+    //IsBlank method, which checks whether the current string is null, empty, or consists of only whitespace characters.
+    public bool IsBlank()
+    {
+        return length == 0;
+    }
+
+    //OnBlank method, which returns the string representation of the current string or the given string if the current value is blank.
+    public string OnBlank(string str)
+    {
+        return IsBlank() ? str : GetString();
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            CustomStringBuilderLinkedList customStringBuilder = new CustomStringBuilderLinkedList();
+            customStringBuilder.Append("aaaa ");
+            customStringBuilder.Append("bbbb").Append("yyyy");
+
+            Console.WriteLine("Length: " + customStringBuilder.Length);
+            Console.WriteLine("Result: " + customStringBuilder.GetString());
+
+            customStringBuilder.InsertAt(" cccc", 5);
+            Console.WriteLine("Result after InsertAt: " + customStringBuilder.GetString());
+
+            customStringBuilder.RemoveDuplicates();
+            Console.WriteLine("Result after RemoveDuplicates: " + customStringBuilder.GetString());
+
+            customStringBuilder.RemoveWhitespaces();
+            Console.WriteLine("Result after RemoveWhitespaces: " + customStringBuilder.GetString());
+
+            Console.WriteLine("IsBlank: " + customStringBuilder.IsBlank());
+
+            string result = customStringBuilder.OnBlank("Default Value");
+            Console.WriteLine("OnBlank: " + result);
+        }
+    }
+
+}
+
+
+
+
